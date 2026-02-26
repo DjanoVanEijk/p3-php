e2637e65ebc9cc29de016ff7e5fd647f679c6461
DjanoVanEijk <9028315@student.zadkine.nl>
2026-02-26 14:40:33 +0100

    hoofdopdracht deel 1

---
+++ hoofdopdracht/index.php
@@ -0,0 +1,28 @@
+<!-- De ECHTE Canvas -->
+
+<?php
+$appNaam = "Kanvas";
+$trackerType = "Huiswerk";
+$Jaar = "Doe je huiswerk of ga good";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <h2><?= $appNaam ?></h2>
+    </header>
+
+    <main>
+        <h1>Hoi, welkom bij <?= $appNaam ?></h1>
+    </main>
+
+    <footer>
+        <?= $appNaam ?> <?php echo date("Y");?>
+    </footer>
+</body>
+</html>
