e522715c473d7051b45c06039b4898c66522da2c
DjanoVanEijk <9028315@student.zadkine.nl>
2026-03-12 15:06:13 +0100

    fabhifbjadfbajokfbaiofhaouifna

--- hoofdopdracht/index.php
+++ hoofdopdracht/index.php
@@ -1,9 +1,22 @@
 <!-- De ECHTE Canvas -->
-
+ 
     <?php require "includes/header.php"; ?>
+    <?php require "includes/db.php"; ?>
 
     <main>
         <h1><span id="greeting">Hoi</span>, welkom bij <?= $appNaam ?></h1>
+
+        <?php
+        $tabel1 = $conn->prepare("SELECT * FROM tabel1");
+        $tabel1->execute();
+        $doosje_met_tabel1 = $tabel1->fetchAll(PDO::FETCH_ASSOC);
+
+        echo "<ul>";
+        foreach ($doosje_met_tabel1 as $tabel1deel2){
+        echo "<li>" . $tabel1['title'] . "</li>";
+        }
+        echo "</ul>";
+        ?>
     </main>
     <?php require "includes/nav.php"; ?>
     <br><br>
