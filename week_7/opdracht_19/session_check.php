be426a3857bff06c502ecda38d1cd3f342e42720
DjanoVanEijk <9028315@student.zadkine.nl>
2026-04-07 11:36:50 +0200

    opdracht_19

---
+++ week_7/opdracht_19/session_check.php
@@ -0,0 +1,16 @@
+<?php
+session_start();
+$_SESSION['kleur'] = 'lila';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Session check</h1>
+    <p>Mijn opgeslagen waarde is: <?php echo $_SESSION['kleur']; ?></p>
+</body>
+</html>
